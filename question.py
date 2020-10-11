5b96b8fc2356adcabb84128ad1bff8ce1d6758a3
Kacper Adach <kadach@hubspot.com>
2020-10-11 11:57:48 -0400

    accept accents

--- question.py
+++ question.py
@@ -10,6 +10,7 @@ import Levenshtein
 import requests
 from bs4 import BeautifulSoup
 from num2words import num2words
+from unidecode import unidecode
 
 STOP_WORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
@@ -61,11 +62,12 @@ class Question:
                 return True
             guess = num2words(guess)
 
-        question = self.question.lower()
+
+        question = unidecode(self.question.lower())
         question = re.sub(r'[^A-Za-z0-9 ]+', '', question)
-        answer = self.answer.lower()
+        answer = unidecode(self.answer.lower())
         answer = re.sub(r'[^A-Za-z0-9 ]+', '', answer)
-        guess = guess.lower()
+        guess = unidecode(guess.lower())
         guess = re.sub(r'[^A-Za-z0-9 ]+', '', guess)
 
         question_tokens = question.split(' ')
@@ -271,5 +273,5 @@ class QuestionDatabase:
         return questions
 
 
-Question_Database = QuestionDatabase(scrape=True)
+Question_Database = QuestionDatabase(scrape=False)
 
